401c53dee3e19a5485743cfb772e64c9e059a496
Rishika1711 <89693269+Rishika1711@users.noreply.github.com>
2021-08-29 12:17:40 +0200

    Update BMI_FI_PI.py

--- BMI_FI_PI.py
+++ BMI_FI_PI.py
@@ -1,3 +1,5 @@
+#Government of Switzerland has decided to check the BMI, FI and the PI.If PI>2, government will provide PI*0.75 times protien. 
+#If FI>2, then government will give 5000$. If FI<1,, 3000$ is awarded and if FI is in btw 1 and 2, 2000$ is awarded.
 def BMIcalculator():
     height=int(input("What is your height in cm?"))
     weight=int(input("What is your weight in kg"))
